ef1cf0e99cf938b20c4f9dfd924d70ab8b9fd422
13657290358 <JaydenCheng@whut.edu.cn>
2021-11-26 15:12:24 +0800

    完善安全性和数据库结构

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask,redirect,session,url_for
 from flask.globals import request
+from flask.helpers import flash
 from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
+import random
+import string
 
 pymysql.install_as_MySQLdb()
 app = Flask(__name__) 
-Flask.secret_key = "87859158897599400jaydenwutisimsom"
+Flask.secret_key = ''.join(random.sample(string.ascii_letters + string.digits, 8))
 db  = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:42289062awsdfG@localhost/sce_db"
 
@@ -15,7 +18,7 @@ class User(db.Model) :
     name = db.Column(db.VARCHAR(20), nullable=0)
     major = db.Column(db.VARCHAR(4) , nullable=0)
     grClass = db.Column(db.CHAR(4) , nullable=0)
-    pwd = db.Column(db.VARCHAR(255) , nullable=0)
+    pwd = db.Column(db.CHAR(32) , nullable=0)
     level = db.Column(db.CHAR(1) , nullable=0 , default= '0')
 
 @app.route('/sce')
@@ -30,7 +33,7 @@ def sec_index() :
 @app.route('/sce/login', methods=['GET', 'POST'])
 def login() :
     if request.method == 'GET' :
-        return render_template('login.html',loginUrl = url_for('login'))
+        return render_template('login.html')
     elif request.method == 'POST' :
         session['uid'] = request.form.get('codetxt') # 获取表单数据
         session['is_log'] = 1
@@ -39,11 +42,11 @@ def login() :
 @app.route('/sce/reg' , methods = ['GET','POST'])
 def reg() :
     if request.method == 'GET' : 
-        return render_template('reg.html', regUrl = url_for('reg'),loginUrl = url_for('login'))
+        return render_template('reg.html')
     elif request.method == 'POST' :
         uid = request.form.get('codetxt')
         if User.query.get(uid) :
-            return redirect('/sce/login')
+            return redirect(url_for('reg'))
         reg_user = User(
             uid = uid ,
             name =  request.form.get('nametxt') ,
@@ -53,7 +56,7 @@ def reg() :
         )
         db.session.add(reg_user)
         db.session.commit()
-        return redirect('/sce/login')
+        return redirect(url_for('login'))
 
 @app.route('/sce/license')
 def license() :

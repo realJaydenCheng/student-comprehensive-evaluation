95b47f5d167fe2ee85cd507b2a22798f6657b45d
13657290358 <JaydenCheng@whut.edu.cn>
2021-11-29 23:18:43 +0800

    完善登录模块

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import random
 import string
-from scepy.reg import *
+from scepy.reg import check_reg_form
+from scepy.login import check_login_form
 
 pymysql.install_as_MySQLdb()
 app = Flask(__name__) 
@@ -14,8 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:42289062awsdfG@localhost/s
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 0
 Flask.secret_key = ''.join(random.sample(string.ascii_letters + string.digits, 8))
 db  = SQLAlchemy(app)
-
-
 
 class User(db.Model) : 
     __tablename__ = "user"
@@ -27,11 +26,10 @@ class User(db.Model) :
     level = db.Column(db.CHAR(1) , nullable=0 , default= '0')
 
 @app.route('/sce')
-@app.route('/home')
-@app.route('/index')
+@app.route('/sce/index')
 def index() :
     if session.get('is_log',0) :
-        username = User.query.filter_by(uid = session['uid']).first()
+        username = User.query.filter_by(uid = session.get('uid', None)).first()
         username = username.name
         return render_template('_features.html' , user_name = username)
     else :
@@ -40,11 +38,25 @@ def index() :
 @app.route('/sce/login', methods=['GET', 'POST'])
 def login() :
     if request.method == 'GET' :
-        return render_template('login.html')
+        if session.get('is_log',0) == 1 :
+            is_exit = request.args.get('exit')
+            if is_exit == '1' :
+                session['is_log'] = 0
+                return redirect(url_for('login'))
+            return redirect(url_for('index'))
+        else :
+            return render_template('login.html')
     elif request.method == 'POST' :
-        session['uid'] = request.form.get('codetxt') # 获取表单数据
-        session['is_log'] = 1
-        return redirect(url_for('index'))
+        form = request.form
+        if check_login_form(form , User.query) :
+            session['uid'] = form.get('codetxt') # 获取表单数据
+            session['is_log'] = 1
+            flash('登陆成功！')
+            return redirect(url_for('index'))
+        else :
+            flash('账号或密码输入不正确，请重试！')
+            return redirect(url_for('login'))
+
 
 @app.route('/sce/reg' , methods = ['GET','POST'])
 def reg() :

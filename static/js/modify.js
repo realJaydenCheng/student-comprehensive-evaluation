21f3ee9182b289b8a949ed80d2cf3ed8fd010127
13657290358 <JaydenCheng@whut.edu.cn>
2021-12-10 09:28:39 +0800

    列表自动添加id

--- static/js/modify.js
+++ static/js/modify.js
@@ -1,12 +1,25 @@
 function addLine() {
     let line = document.getElementById('templine').cloneNode(true);
     let list = document.getElementById('relist');
-    line.style.display = 'table-row' ;
+    line.style.display = 'table-row';
     list.appendChild(line);
+    changeIndex();
 }
 
 function delLine(n) {
     let list = document.getElementById('relist');
     let line = n.parentNode.parentNode;
     list.removeChild(line);
+    changeIndex();
+}
+
+function changeIndex() {
+    let list = document.getElementById('relist');
+    let lines = list.getElementsByTagName("tr");
+    for (let i = 0; i <= lines.length; i++) {
+        let line = lines[i];
+        let index = line.getElementsByTagName("td");
+        index = index[0];
+        index.innerText = i;
+    }
 }

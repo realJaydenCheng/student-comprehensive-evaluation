a8bf35f85b98e4d14fe48e808a47236e6df84f9b
13657290358 <JaydenCheng@whut.edu.cn>
2021-12-10 19:33:35 +0800

    完成前端的奖励分信息获取部分

--- static/js/modify.js
+++ static/js/modify.js
@@ -16,10 +16,38 @@ function delLine(n) {
 function changeIndex() {
     let list = document.getElementById('relist');
     let lines = list.getElementsByTagName("tr");
-    for (let i = 0; i <= lines.length; i++) {
+    for (let i = 0; i <= lines.length - 1; i++) {
         let line = lines[i];
         let index = line.getElementsByTagName("td");
         index = index[0];
         index.innerText = i;
     }
+}
+
+function beforeModify() {
+    if (beforeLogin()) {
+        var rewards = document.getElementById("rewards");
+        var idTxt = '';
+        var valueTxt = '';
+        var noteTxt = '';
+        let rewardList = document.getElementById('relist');
+        rewardList = rewardList.getElementsByTagName('tr');
+        for (let i = 1; i <= rewardList.length - 1; i++) {
+            let reward = rewardList[i];
+            reward = reward.getElementsByTagName('td');
+            if (i == rewardList.length - 1) {
+                idTxt = idTxt + reward[0].innerText;
+                valueTxt = valueTxt + reward[1].getElementsByTagName('input')[0].value;
+                noteTxt = noteTxt + reward[2].getElementsByTagName('input')[0].value;
+            }
+            else{
+                idTxt = idTxt + reward[0].innerText + "|next_item_is|";
+                valueTxt = valueTxt + reward[1].getElementsByTagName('input')[0].value + "|next_item_is|";
+                noteTxt = noteTxt + reward[2].getElementsByTagName('input')[0].value + "|next_item_is|";
+            }
+        }
+        rewards.value = idTxt + "/*a_new_line*/" + valueTxt + "/*a_new_line*/" + noteTxt;
+        return true;
+    }
+    else { return false; }
 }

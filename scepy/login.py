af8f231a0d069ae692a73783347d3b7fd4833773
13657290358 <JaydenCheng@whut.edu.cn>
2021-12-31 09:28:00 +0800

    修复账号不存在时抛出错误的bug

--- scepy/login.py
+++ scepy/login.py
@@ -2,6 +2,8 @@ def check_login_form(form, db):
     login_id = form.get('codetxt')
     login_pwd = form.get('pwdtxt')
     true_pwd = db.filter_by(uid=login_id).first()
+    if not true_pwd :
+        return 0
     true_pwd = true_pwd.pwd
     if true_pwd == login_pwd:
         return 1

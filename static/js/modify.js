06ffaf7ad24963cd3fa38b77d1dcd500e7fa2c7f
13657290358 <JaydenCheng@whut.edu.cn>
2021-12-31 13:09:03 +0800

    完善前端显示，增加增改页面的信息审核功能，修复最低绩点可以大于平均绩点的bug

--- static/js/modify.js
+++ static/js/modify.js
@@ -1,11 +1,22 @@
+function changeIndex() {
+    let list = document.getElementById('relist');
+    let lines = list.getElementsByTagName("tr");
+    for (let i = 0; i <= lines.length - 1; i++) {
+        let line = lines[i];
+        let index = line.getElementsByTagName("td");
+        index = index[0];
+        index.innerText = i;
+    }
+}
+
 function addLine() {
     let line = document.getElementById('templine').cloneNode(true);
     let list = document.getElementById('relist');
-    let inputs = line.getElementsByTagName('input') ;
-    let num = inputs[0] ;
-    let txt = inputs[1] ; 
-    num.required = true ;
-    txt.required = true ;
+    let inputs = line.getElementsByTagName('input');
+    let num = inputs[0];
+    let txt = inputs[1];
+    num.required = true;
+    txt.required = true;
     line.style.display = 'table-row';
     list.appendChild(line);
     changeIndex();
@@ -18,38 +29,83 @@ function delLine(n) {
     changeIndex();
 }
 
-function changeIndex() {
-    let list = document.getElementById('relist');
-    let lines = list.getElementsByTagName("tr");
-    for (let i = 0; i <= lines.length - 1; i++) {
-        let line = lines[i];
-        let index = line.getElementsByTagName("td");
-        index = index[0];
-        index.innerText = i;
+function checkNum(elem, low, heigh) {
+    let tip = elem.parentNode.parentNode.getElementsByClassName("tip")[0];
+    if ((elem.value >= low) && (elem.value <= heigh) && (elem.value != "")) {
+        tip.style.color = 'rgb(128,128,128)';
+        return true;
+    }
+    else {
+        tip.style.color = 'red';
+        return false;
     }
 }
 
 function beforeModify() {
     if (beforeLogin()) {
-        let isOk = true ;
+        let pwd = document.getElementById('pwdtxt');
+        let isOk = true;
         let infos = document.getElementsByClassName('infoinput');
-        for (let i =0 ; i < infos.length ; i++){
-            if ((i == 6)||(i == 8)){continue;}
-            let info = infos[i] ;
-            if (!checkInfo(info)){
-                isOk = false ;
-                break ;
+        for (let i = 0; i < infos.length; i++) {
+            if ((i == 6) || (i == 8)) { continue; }
+            let info = infos[i];
+            if (!checkInfo(info)) {
+                isOk = false;
+                break;
             }
         }
-        if (!isOk){
-            window.alert("请检查标红处的数据是否填写错误。") ;
-            let pwd = document.getElementById('pwdtxt');
-            pwd.value = '' ;
+        if (!isOk) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = '';
             return false;
         }
 
         let job = document.getElementById("jobtxt");
-        if (!checkJob(job)) {return false;}
+        if (!checkJob(job)) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = ''; return false;
+        }
+
+        let mor = document.getElementById("mortxt");
+        let gpa = document.getElementById("gpatxt");
+        let c1 = document.getElementById("c1");
+        let my = document.getElementById("mytxt");
+        let ld = document.getElementById("ldtxt");
+        let dis = document.getElementById("distxt");
+        let low = document.getElementById("low");
+        let cre = document.getElementById("cretxt");
+        if (!checkNum(mor, 3.2, 15)) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = ''; return false;
+        }
+        if (!checkNum(gpa, 0, 5)) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = ''; return false;
+        }
+        if (!checkNum(c1, 0, 100)) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = ''; return false;
+        }
+        if (!checkNum(my, 1.4, 5)) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = ''; return false;
+        }
+        if (!checkNum(ld, 1.4, 5)) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = ''; return false;
+        }
+        if (!checkNum(dis, 0, 10)) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = ''; return false;
+        }
+        if (!checkNum(low, 0, gpa.value)) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = ''; return false;
+        }
+        if (!checkNum(cre, 20, 80)) {
+            window.alert("请检查标红处的数据是否填写错误。");
+            pwd.value = ''; return false;
+        }
 
         var rewards = document.getElementById("rewards");
         var idTxt = '';
@@ -72,14 +128,14 @@ function beforeModify() {
             }
         }
         rewards.value = idTxt + "/*a_new_line*/" + valueTxt + "/*a_new_line*/" + noteTxt;
-        window.onbeforeunload = null ;
+        window.onbeforeunload = null;
         return true;
     }
     else { return false; }
 }
 
 function checkInfo(elem) {
-    let tip = elem.parentNode.parentNode.getElementsByClassName("tip")[0] ;
+    let tip = elem.parentNode.parentNode.getElementsByClassName("tip")[0];
     if (elem.value != '') {
         tip.style.color = 'rgb(128,128,128)';
         return true;
@@ -92,25 +148,30 @@ function checkInfo(elem) {
 }
 
 function checkJob(elem) {
-    let tip = elem.parentNode.parentNode.getElementsByClassName("tip")[0] ;
-    if ((elem.value.length < 50) && (elem.value != '') ) {
+    let tip = elem.parentNode.parentNode.getElementsByClassName("tip")[0];
+    if ((elem.value.length < 50) && (elem.value != '')) {
         tip.style.color = 'rgb(128,128,128)';
         return true;
     }
     else {
         tip.style.color = 'red';
-        elem.value = elem.value.slice(0,10) ;
+        elem.value = elem.value.slice(0, 10);
         return false;
     }
 }
 
 function checkNote(elem) {
-    if (elem.value.length < 50 ) {
+    if (elem.value.length < 50) {
         return true;
     }
     else {
-        elem.value = elem.value.slice(0,50) ;
+        elem.value = elem.value.slice(0, 50);
         window.alert("奖励分备注最多五十字符！");
         return false;
     }
+}
+
+function changeLow(low) {
+    let lown = document.getElementById("low");
+    lown.setAttribute('max', low);
 }
